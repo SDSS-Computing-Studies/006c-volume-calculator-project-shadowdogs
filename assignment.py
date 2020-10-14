fbf52aacb246cd9fc67ba20a6db57bae0f9b6272
JhuneCastillo <71406970+JhuneCastillo@users.noreply.github.com>
2020-10-14 12:44:48 -0700

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -7,10 +7,19 @@ def title():
     # Will display a title screen
     # input parameters: none needed
     # output parameters: None
-    # Author:
+    # Author: Jhune Francis
     # Modified:
-    
+    print("""
+    ----------Welcome To ShadowDogsUnleashed Calculator----------
+    Cube
+    Sphere
+    Cone
+    Cylinder
+    Pyramid 
+    -------------------------------------------------------------
+    """)
     return None
+
 
 def instructions():
     # Will display instructions
@@ -18,17 +27,31 @@ def instructions():
     # output parameters: None
     # Author:
     # Modified:
+    print("""
+    ----------Instructions----------
+    Choose a shape listed above
+    Enter value of the shape that you chose
+
+    """)
+
     return None
+
 
 def getParams(shape):
     # Will create a list of questions to be asked depending on the shape.
     # These will be asked so that the user can enter in appropriate values
-    # input parameter: string 
+    # input parameter: string
     # output parameter: return a list containing the prompts for each shape:
     # example: ["Enter the radius:","Enter the slant height:","Enter the height:"]
+<<<<<<< Updated upstream
     if shape == "Cube":
         # volume = sidelength**3
         paramList = ["Enter a side length: "]
+=======
+    # Author:
+    # Modified:
+    prompts
+>>>>>>> Stashed changes
 
     elif shape == "Sphere":
         # volume = (4 / 3) * math.pi * (radius**3)
@@ -48,20 +71,27 @@ def getParams(shape):
 
     return paramList
 
+
 def getInputs(questions):
     # Will prompt the user for inputs for the shape they.
     # These will be asked so that the user can enter in appropriate values
     # It will turn all the input data into a list
     # input parameter: list containing the prompts/questions
     # output parameter: return a list containing all the measurements of the shape
+    # Author:
+    # Modified:
     measurements
-    
+
     return measurements
+
 
 def main():
     # main block of code that will run your program and control program flow
     # You will need to include a while loop to keep repeating the commands until
     # the user chooses to exit
+    # Author:
+    # Modified:
     title()
+
 
 main()
